97500bd68b583c532a45e47bd2b121a4a382cfaf
Stas Ivanov <hubbahubbawtf@gmail.com>
2015-07-22 20:10:13 +0400

    4th commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,34 @@
 // variables
 var gulp = require('gulp'),
-	concatCss = require('gulp-concat-css'),
+	prefix = require('gulp-autoprefixer'),
 	minifyCss = require('gulp-minify-css'),
 	rename = require('gulp-rename'),
-	livereload = require('gulp-livereload'),
+	concatCss = require('gulp-concat-css'),
 	scssCss = require('gulp-sass'),
-	prefix = require('gulp-autoprefixer'),
-	connect = require('gulp-connect'),
 	slim = require('gulp-slim'),
 	spritesmith = require('gulp.spritesmith');
 
-// server connect and livereload
-gulp.task('connect', function() {
-	connect.server ({
-		root: 'Project-00',
-		livereload: true
-	})
-});
+// // server connect and livereload
+// gulp.task('connect', function() {
+// 	connect.server ({
+// 		root: 'Project-00',
+// 		livereload: true
+// 	})
+// });
 
-// doing nothing
-gulp.task('html', function() {
-	gulp.src('index.html')
-	.pipe(connect.reload());
-});
+// // doing nothing
+// gulp.task('html', function() {
+// 	gulp.src('index.html')
+// 	.pipe(connect.reload());
+// });
 
-// compiling scss to css
-gulp.task('scssCss', function() {
-	gulp.src('scss/*.scss')
-		.pipe(scssCss())
-		.pipe(gulp.dest('./css'))
-		.pipe(connect.reload());
-});
+// // compiling scss to css
+// gulp.task('scssCss', function() {
+// 	gulp.src('scss/*.scss')
+// 		.pipe(scssCss())
+// 		.pipe(gulp.dest('./css'))
+// 		.pipe(connect.reload());
+// });
 
 // prefixing css
 gulp.task('prefix', function() {
